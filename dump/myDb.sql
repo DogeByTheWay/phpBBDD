924fa512f8315f2aa9b350d57d55558039a66575
Esteban Adarme <tutecnotutos911@gmail.com>
2022-11-18 09:54:22 +0100

    Script de sql actualizado

--- dump/myDb.sql
+++ dump/myDb.sql
@@ -1,22 +1,72 @@
-CREATE DATABASE IF NOT EXISTS myDb;
-USE myDb;
-SET
-  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET
-  time_zone = "+01:00";
-  /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-  /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-  /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-CREATE TABLE `Person` (
-    `id` int(11) NOT NULL,
-    `name` varchar(20) NOT NULL
-  ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
-INSERT INTO
-  `Person` (`id`, `name`)
-VALUES
-  (1, 'Conf'),
-  (2, 'Nipsu'),
-  (3, 'Beyar');
-  /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-  /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-  /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 5.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-11-2022 a las 09:34:38
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `peliculas`
+--
+CREATE DATABASE IF NOT EXISTS peliculas;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas`
+--
+
+CREATE TABLE `peliculas` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `anyo` int(11) NOT NULL,
+  `duracion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `peliculas`
+--
+
+INSERT INTO `peliculas` (`id`, `titulo`, `anyo`, `duracion`) VALUES
+(1, 'El padrino', 1972, 175),
+(2, 'El padrino 2', 1974, 200),
+(3, 'Senderos de gloria', 1957, 86),
+(4, 'Primera Plana', 1974, 105),
+(29, 'pruebass', 1999, 300),
+(30, 'pruebass', 1999, 300);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
